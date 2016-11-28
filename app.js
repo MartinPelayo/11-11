0a7117ae44d99217f0ba1c9d51698f3e8049318f
Martin <martinconradpelayo@gmail.com>
2016-11-27 19:53:55 -0800

    p5

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum (a,b) { //eslint-disable-line
-  return [a + b, 'The sum of ' + a + ' and ' + b + ' is ' + (a + b) + '.'];
+function sum(a,b) {//eslint-disable-line
+  var total = a + b;
+  var string = 'The sum of ' + a + ' and ' + b + ' is ' + total + '.';
+
+  return [total, string];
 }
+
+
+
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -26,7 +32,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-  return [a + b, 'The sum of ' * a * ' and ' * b * ' is ' (a + b) * '.'];
+  var total = a * b;
+  var string = 'The product of ' + a + ' and ' + b + ' is ' + total + '.';
+
+  return[total, string];
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -47,11 +57,23 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
+  var sumTotal = sum(a, b)[0];
+  sumTotal = sum(sumTotal, c)[0];
+
+  var productTotal = multiply(a,b)[0];
+  productTotal = multiply(productTotal, c)[0];
+
+  var sumString = a + ' and ' + b + ' and ' + c + ' sum to ' + sumTotal + '.';
+
+  var productString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productTotal + '.';
+
+  return[sumTotal, productTotal, sumString, productString];
 
 }
 
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -69,12 +91,23 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
+  var total = 0;
+  var string = '';
 
+  for (var i = 0; i < testArray.length; i++){
+    total = sum(total, testArray[i])[0];
+    if (i > 0) string += ',';
+    string += testArray[i];
+  }
+  string += ' was passed in as an array of numbers, and ' + total + ' is their sum.';
+
+
+  return[total,string];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -90,10 +123,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
+  var total = 1;
+  var string = 'The numbers ';
+
+  for (var i = 0; i < testArray.length; i++){
+    total = multiply(total, testArray[i])[0];
+    if (i > 0) string += ',';
+    string += testArray[i];
+  }
+
+  string += ' have a product of ' + total + '.';
+
+  return[total,string];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
